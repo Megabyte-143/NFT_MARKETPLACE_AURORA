25fdaa1ba031be358a316f07a35194d2c9c0b0fd
Yash Garg <yashygkf@gmail.com>
2022-05-24 08:53:13 +0530

    edit: new contract created

--- client/nft-marketplace/src/config.js
+++ client/nft-marketplace/src/config.js
@@ -1,5 +1,5 @@
-export const nftAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const nftAddress = "0xa4f4d11D9439c430e3621E4510Ec1774C5E75796";
 
-export const nftMarketAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const nftMarketAddress = "0x1B1dBb9C0d46b0D57792Ea4fb55793d442C93D2b";
 
 export const rpc_url = `https://rpc-mumbai.maticvigil.com/v1/3563bdf92c4890518396bd186677a6bc37e727f3`;
